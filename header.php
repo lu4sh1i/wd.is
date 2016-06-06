db5d458f401982c9516a117cf45c017c3f5b0e66
Lu4sh1i <lupu.pavel@ymail.com>
2016-06-06 13:42:56 +0300

    initial commit

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="Where Design" />
+
+    <!-- Stylesheets
+    ============================================= -->
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Playfair+Display:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="<?= isset($STYLECSS) ? $STYLECSS : 'style.css' ?>" type="text/css" />
+    <link rel="stylesheet" href="<?= isset($SWIPER) ? $SWIPER : '/swiper.css'?>" type="text/css" />
+    <link rel="stylesheet" href="<?= isset($DARK) ? $DARK :'css/dark.css'?>" type="text/css" />
+    <link rel="stylesheet" href="<?= isset($FONTICONS) ? $FONTICONS :'css/font-icons.css'?>" type="text/css" />
+    <link rel="stylesheet" href="<?= isset($ANIMATE) ? $ANIMATE :'css/animate.css'?>" type="text/css" />
+    <link rel="stylesheet" href="<?= isset($MAGNIFIC) ? $MAGNIFIC :'css/magnific-popup.css'?>" type="text/css" />
+
+    <link rel="stylesheet" href="<?= isset($RESPONSIVE) ? $RESPONSIVE : 'css/responsive.css'?>" type="text/css" />
+    <link rel="stylesheet" href="<?= isset($COLORS) ? $COLORS : 'css/colors.css'?>" type="text/css" />
+    <link rel="stylesheet" href="<?= isset($CUSTOM) ? $CUSTOM : 'css/custom.css'?>" type="text/css" />
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lt IE 9]>
+    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
+
+    <!-- Document Title
+    ============================================= -->
+    <title><?= isset($TITTLE) ? $TITLE : "Where Design is "?></title>
+
+    <?php if (function_exists('customPageHeader')){
+        customPageHeader();
+    }?>
+
+</head>
+
+<body class="stretched">
---
+++ nav.php
@@ -0,0 +1,34 @@
+<header id="header" class="transparent-header">
+
+    <div id="header-wrap">
+
+        <div class="container clearfix">
+
+            <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+            <!-- Logo
+            ============================================= -->
+            <div id="logo">
+                <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>
+                <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+            </div><!-- #logo end -->
+
+            <!-- Primary Navigation
+            ============================================= -->
+            <nav id="primary-menu">
+
+                <ul>
+                    <li class="current"><a href="index.html"><div>Studio</div></a>
+                        <ul>
+                            <li><a href="index-corporate.html"><div>Home - Corporate</div></a></li>
+                            <li><a href="index-corporate.html"><div>Home - Corporate</div></a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#"><div>Works</div></a>   </li>
+                    <li><a href="#"><div>Services</div></a> </li>
+                    <li><a href="#"><div>Contact</div></a> </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
