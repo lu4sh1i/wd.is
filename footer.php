fe85c3e30246cc3c655b0ddfce967a559fa15915
Pavlic <lupu.pavel@ymail.com>
2016-07-21 16:40:35 +0300

    added social links

--- footer.php
+++ footer.php
@@ -47,7 +47,7 @@
 
                 <div class="col_one_third col_last">
 
-                    <a href="#" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+                    <a href="https://www.facebook.com/wheredesign.is/" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
                         <i class="icon-facebook"></i>
                         <i class="icon-facebook"></i>
                     </a>
@@ -110,42 +110,27 @@
 
             <div class="col_half col_last tright">
                 <div class="fright clearfix">
-                    <a href="#" class="social-icon si-small si-borderless si-facebook">
+                    <a href="https://www.facebook.com/wheredesign.is/" target="_blank" class="social-icon si-small si-borderless si-facebook">
                         <i class="icon-facebook"></i>
                         <i class="icon-facebook"></i>
                     </a>
 
-                    <a href="#" class="social-icon si-small si-borderless si-twitter">
+                    <a href="https://twitter.com/wheredesignis" target="_blank" class="social-icon si-small si-borderless si-twitter">
                         <i class="icon-twitter"></i>
                         <i class="icon-twitter"></i>
                     </a>
 
-                    <a href="#" class="social-icon si-small si-borderless si-gplus">
+                    <a href="https://plus.google.com/+WhereDesignBucure%C8%99ti" class="social-icon si-small si-borderless si-gplus">
                         <i class="icon-gplus"></i>
                         <i class="icon-gplus"></i>
                     </a>
 
-                    <a href="#" class="social-icon si-small si-borderless si-pinterest">
+                    <a href="https://pinterest.com/wheredesigner" class="social-icon si-small si-borderless si-pinterest">
                         <i class="icon-pinterest"></i>
                         <i class="icon-pinterest"></i>
                     </a>
 
-                    <a href="#" class="social-icon si-small si-borderless si-vimeo">
-                        <i class="icon-vimeo"></i>
-                        <i class="icon-vimeo"></i>
-                    </a>
-
-                    <a href="#" class="social-icon si-small si-borderless si-github">
-                        <i class="icon-github"></i>
-                        <i class="icon-github"></i>
-                    </a>
-
-                    <a href="#" class="social-icon si-small si-borderless si-yahoo">
-                        <i class="icon-yahoo"></i>
-                        <i class="icon-yahoo"></i>
-                    </a>
-
-                    <a href="#" class="social-icon si-small si-borderless si-linkedin">
+                    <a href="https://www.linkedin.com/company/where-design" class="social-icon si-small si-borderless si-linkedin">
                         <i class="icon-linkedin"></i>
                         <i class="icon-linkedin"></i>
                     </a>
